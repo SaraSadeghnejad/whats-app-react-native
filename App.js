e375727960544245d4d7b3ac89b2ef631c05693a
sara.sadeghnezhad <sarasadeghnejad@yahoo.com>
2023-04-07 18:59:57 +0430

    add messagges

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ChatListItem from './src/components/ChatListItem';
 import ChatScreen from './src/screens/ChatScreen';
+import ChatsScreen from './src/screens/ChatsScreen';
 
 export default function App() {
   return (
     <View>
- <ChatScreen />
+    <ChatsScreen />
+ {/* <ChatScreen /> */}
     </View>
   );
 }
